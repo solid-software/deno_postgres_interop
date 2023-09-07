1c505491f94b8ad40383a90122abe3a336e4f5fc
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-07 13:48:19 +0300

    upd

--- example/compile_and_run.sh
+++
@@ -1,3 +0,0 @@
-sh compile_supabase_function.sh
-mv -f functions/dart_edge/* ../../supabase/functions/dart_edge/
-supabase functions serve --no-verify-jwt
--- example/compile_supabase_function.sh
+++
@@ -1,2 +0,0 @@
-dart run edge build supabase_functions
-sh fix_js_output.sh
