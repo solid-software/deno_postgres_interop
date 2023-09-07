9b293eb0e6a24ea609ca687b5cedc7c9909a5855
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-07 18:37:20 +0300

    add comments

--- lib/src/query_client.dart
+++ lib/src/query_client.dart
@@ -1,4 +1,3 @@
-// https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient
 import 'dart:js_interop';
 import 'dart:js_util';
 
@@ -6,16 +5,25 @@ import 'package:deno_postgres_interop/src/query_object_result.dart';
 import 'package:deno_postgres_interop/src/transaction.dart';
 import 'package:deno_postgres_interop/src/util.dart';
 
+/// [postgres@v0.17.0/QueryClient](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient).
 @JS()
 class QueryClient {}
 
+/// [postgres@v0.17.0/QueryClient](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient).
 extension QueryClientProps on QueryClient {
+  /// [postgres@v0.17.0/QueryClient/connect](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_connect_0).
   Future<void> connect() => callFutureMethod(this, 'connect');
+
+  /// [postgres@v0.17.0/QueryClient/end](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_end_0).
   Future<void> end() => callFutureMethod(this, 'end');
 
+  /// [postgres@v0.17.0/QueryClient/createTransaction](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_createTransaction_0).
   Transaction createTransaction(String name) =>
       callMethod(this, 'createTransaction', [name]);
 
+  /// Convinience wrapper for [createTransaction],
+  /// [TransactionProps.begin],
+  /// and [TransactionProps.commit].
   Future<T> transaction<T>(
     String name,
     Future<T> Function(Transaction) f,
@@ -28,6 +36,7 @@ extension QueryClientProps on QueryClient {
     return result;
   }
 
+  /// [postgres@v0.17.0/QueryClient/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_0).
   Future<QueryObjectResult<T>> queryObject<T>(String query) => callFutureMethod(
         this,
         'queryObject',
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,5 +1,6 @@
 import 'dart:js_util';
 
+/// Convinience wrapper for [promiseToFuture] and [callMethod].
 Future<T> callFutureMethod<T>(
   Object o,
   Object method, [
