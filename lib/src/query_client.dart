209977ff403944577d709ee4429c220858ae45de
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-14 14:05:58 +0300

    add placeholders

--- lib/src/query_client.dart
+++ lib/src/query_client.dart
@@ -20,11 +20,15 @@ extension QueryClientProps on QueryClient {
   /// [postgres@v0.17.0/QueryClient/connected](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#accessor_connected)
   bool get isConnected => getProperty(this, 'connected');
 
+  /// [postgres@v0.17.0/QueryClient/closeConnection](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_closeConnection_0)
+  Future<void> closeConnection() => callFutureMethod(this, 'closeConnection');
+
+  /// [postgres@v0.17.0/QueryClient/resetSessionMetadata](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_resetSessionMetadata_0)
+  Future<void> resetSessionMetadata() =>
+      callFutureMethod(this, 'resetSessionMetadata');
+
   /// [postgres@v0.17.0/QueryClient/connect](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_connect_0).
   Future<void> connect() => callFutureMethod(this, 'connect');
-
-  /// [postgres@v0.17.0/QueryClient/end](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_end_0).
-  Future<void> end() => callFutureMethod(this, 'end');
 
   /// [postgres@v0.17.0/QueryClient/createTransaction](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_createTransaction_0).
   Transaction createTransaction(String name, [TransactionOptions? options]) =>
@@ -36,6 +40,42 @@ extension QueryClientProps on QueryClient {
           if (options != null) options,
         ],
       );
+
+  /// [postgres@v0.17.0/QueryClient/end](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_end_0).
+  Future<void> end() => callFutureMethod(this, 'end');
+
+  // queryArray<T extends Array<unknown>>(
+  //   query: string,
+  //   args?: QueryArguments,
+  // ): Promise<QueryArrayResult<T>>
+  // https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryArray_0
+
+  // queryArray<T extends Array<unknown>>(
+  //   config: QueryOptions
+  // ): Promise<QueryArrayResult<T>>
+  // https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryArray_1
+
+  // queryArray<T extends Array<unknown>>(
+  //   strings: TemplateStringsArray,
+  //   ...args: unknown[],
+  // ): Promise<QueryArrayResult<T>>
+  // https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryArray_2
+
+  /// [postgres@v0.17.0/QueryClient/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_0).
+  Future<QueryObjectResult<T>> queryObject<T>(
+    String query, [
+    QueryArguments? arguments,
+  ]) =>
+      queryObjectCommon(this, query, arguments);
+
+  // queryObject<T>(config: QueryObjectOptions): Promise<QueryObjectResult<T>>
+  // https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_1
+
+  // queryObject<T>(
+  //   query: TemplateStringsArray,
+  //   ...args: unknown[],
+  // ): Promise<QueryObjectResult<T>>
+  // https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_2
 
   /// Convinience wrapper for [createTransaction],
   /// [TransactionProps.begin],
@@ -52,11 +92,4 @@ extension QueryClientProps on QueryClient {
 
     return result;
   }
-
-  /// [postgres@v0.17.0/QueryClient/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_0).
-  Future<QueryObjectResult<T>> queryObject<T>(
-    String query, [
-    QueryArguments? arguments,
-  ]) =>
-      queryObjectCommon(this, query, arguments);
 }
