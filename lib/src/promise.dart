d14616ddb3108a98931512053c50ef4fc140fdf6
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-15 13:41:56 +0300

    promise docs

--- lib/src/promise.dart
+++ lib/src/promise.dart
@@ -1,17 +1,17 @@
 import 'dart:js_interop';
 import 'dart:js_util';
 
-typedef Resolver<T> = void Function(T result);
+typedef _Resolver<T> = void Function(T result);
+typedef _Executor<T> = void Function(_Resolver<T> resolve, Function reject);
 
-typedef Executor<T> = void Function(Resolver<T> resolve, Function reject);
-
+/// JS [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) interop.
 @JS()
 class Promise<T> {
-  external Promise(Executor<T> executor);
-
-  external Promise then(Resolver<T> onFulfilled, [Function onRejected]);
+  /// [js/Promise/constructor](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/Promise).
+  external Promise(_Executor<T> executor);
 }
 
+/// Convert darts [Future] to js' [Promise].
 Promise<T> futureToPromise<T>(Future<T> future) {
   return Promise<T>(
     allowInterop((resolve, reject) {
