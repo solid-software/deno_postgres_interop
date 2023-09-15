75f51e3a7f3a294a78eb5f183edfebf55a99f794
danylo-safonov-solid <116712879+danylo-safonov-solid@users.noreply.github.com>
2023-09-15 13:31:57 +0300

    fix comments (#8)

--- lib/src/isolation_level.dart
+++ lib/src/isolation_level.dart
@@ -1,12 +1,12 @@
-/// https://www.postgresql.org/docs/current/transaction-iso.html
+/// [postgresql/Transaction Isolation](https://www.postgresql.org/docs/current/transaction-iso.html).
 enum IsolationLevel {
-  /// [postgresql/Transaction Isolation/Read Committed Isolation Level](https://www.postgresql.org/docs/current/transaction-iso.html#XACT-READ-COMMITTED)
+  /// [postgresql/Transaction Isolation/Read Committed Isolation Level](https://www.postgresql.org/docs/current/transaction-iso.html#XACT-READ-COMMITTED).
   readCommitted,
 
-  /// [postgresql/Transaction Isolation/Repeatable Read Isolation Level](https://www.postgresql.org/docs/current/transaction-iso.html#XACT-REPEATABLE-READ)
+  /// [postgresql/Transaction Isolation/Repeatable Read Isolation Level](https://www.postgresql.org/docs/current/transaction-iso.html#XACT-REPEATABLE-READ).
   repeatableRead,
 
-  /// [postgresql/Transaction Isolation/Serializable Isolation Level](https://www.postgresql.org/docs/current/transaction-iso.html#XACT-SERIALIZABLE)
+  /// [postgresql/Transaction Isolation/Serializable Isolation Level](https://www.postgresql.org/docs/current/transaction-iso.html#XACT-SERIALIZABLE).
   serializable;
 
   /// Parses a string containing an [IsolationLevel] literal into its instance.
