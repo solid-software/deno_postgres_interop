c1f6a1d2f0b085ef594e099e6dbc89dca6ec2868
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-18 18:51:19 +0300

    ResultType

--- lib/src/result_type.dart
+++ lib/src/result_type.dart
@@ -1,1 +1,12 @@
-class ResultType {}
+/// [postgres@v0.17.0/ResultType](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=ResultType).
+enum ResultType {
+  /// array.
+  array,
+
+  /// object.
+  object;
+
+  /// Parses a string containing an [ResultType] literal into its instance.
+  static ResultType parse(String string) =>
+      values.firstWhere((e) => e.name.toUpperCase() == string);
+}
