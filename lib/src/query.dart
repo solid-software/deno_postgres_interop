8075339194cc1b4d1f5b90f457e99c5111ddc604
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-18 22:23:00 +0300

    remove generic

--- lib/src/query.dart
+++ lib/src/query.dart
@@ -5,10 +5,10 @@ import 'package:deno_postgres_interop/src/result_type.dart';
 
 /// [deno-postgres@v0.17.0/Query](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=Query).
 @JS()
-class Query<T extends ResultType> {}
+class Query {}
 
 /// [deno-postgres@v0.17.0/Query](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=Query).
-extension QueryProps<T extends ResultType> on Query {
+extension QueryProps on Query {
   /// [deno-postgres@v0.17.0/Query/result_type](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=Query#prop_result_type).
   ResultType get resultType =>
       ResultType.values[getProperty(this, 'result_type')];
--- lib/src/query_result.dart
+++ lib/src/query_result.dart
@@ -3,13 +3,12 @@ import 'dart:js_util';
 
 import 'package:deno_postgres_interop/src/command_type.dart';
 import 'package:deno_postgres_interop/src/query.dart';
-import 'package:deno_postgres_interop/src/result_type.dart';
 
 /// [deno-postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
 @JS()
 class QueryResult {
   /// [deno-postgres@v0.17.0/QueryResult/constructor](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult#ctor_0).
-  external Query<ResultType> get query;
+  external Query get query;
 }
 
 /// [deno-postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
