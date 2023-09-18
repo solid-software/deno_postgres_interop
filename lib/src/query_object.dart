c92dd81962ad856981e84e11cfcf9d35b96e8fe6
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-18 19:54:32 +0300

    more consistent comments

--- lib/src/query_object.dart
+++ lib/src/query_object.dart
@@ -1,13 +1,13 @@
 import 'package:deno_postgres_interop/deno_postgres_interop.dart';
 import 'package:deno_postgres_interop/src/util.dart';
 
-/// [postgres@v0.17.0/QueryArguments](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryArguments).
+/// [deno-postgres@v0.17.0/QueryArguments](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryArguments).
 typedef QueryArguments = Object;
 
 /// Common method between all Clients.
 ///
-/// [postgres@v0.17.0/Transaction/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Transaction#method_queryObject_0).
-/// [postgres@v0.17.0/QueryClient/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_0).
+/// [deno-postgres@v0.17.0/Transaction/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Transaction#method_queryObject_0).
+/// [deno-postgres@v0.17.0/QueryClient/queryObject](https://deno.land/x/postgres@v0.17.0/mod.ts?s=QueryClient#method_queryObject_0).
 Future<QueryObjectResult<T>> queryObjectCommon<T>(
   Object queryable,
   String query, [
--- lib/src/query_result.dart
+++ lib/src/query_result.dart
@@ -3,13 +3,13 @@ import 'dart:js_util';
 
 import 'package:deno_postgres_interop/src/command_type.dart';
 
-/// [postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
+/// [deno-postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
 @JS()
 class QueryResult {}
 
-/// [postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
+/// [deno-postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
 extension QueryResultProps on QueryResult {
-  /// [postgres@v0.17.0/QueryResult/command](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult#prop_command).
+  /// [deno-postgres@v0.17.0/QueryResult/command](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult#prop_command).
   CommandType get command => CommandType.parse(
         getProperty(this, 'command'),
       );
