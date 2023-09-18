d3f8d9056d0a29d30af66328b21eeb0600541673
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-18 19:51:19 +0300

    working

---
+++ lib/src/query_result.dart
@@ -0,0 +1,16 @@
+import 'dart:js_interop';
+import 'dart:js_util';
+
+import 'package:deno_postgres_interop/src/command_type.dart';
+
+/// [postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
+@JS()
+class QueryResult {}
+
+/// [postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
+extension QueryResultProps on QueryResult {
+  /// [postgres@v0.17.0/QueryResult/command](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult#prop_command).
+  CommandType get command => CommandType.parse(
+        getProperty(this, 'command'),
+      );
+}
