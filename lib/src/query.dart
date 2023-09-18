da3208f0ca5fbaef3018f234c5b9c745b88e68d5
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-18 19:23:44 +0300

    placeholders

--- lib/src/query.dart
+++ lib/src/query.dart
@@ -1,1 +1,35 @@
-class Query<T> {}
+import 'dart:js_interop';
+
+import 'package:deno_postgres_interop/src/result_type.dart';
+
+@JS()
+class Query<T extends ResultType> {
+//   new
+// Query(config: QueryObjectOptions, result_type: T)
+
+// new
+// Query(
+// text: string,
+// result_type: T,
+// args?: QueryArguments,
+// )
+
+// new
+// Query(
+// config_or_text: string | QueryObjectOptions,
+// result_type: T,
+// args?: QueryArguments,
+// )
+
+// args: EncodedArg[]
+
+// optional
+// camelcase: boolean
+
+// optional
+// fields: string[]
+
+// result_type: ResultType
+
+// text: string
+}
