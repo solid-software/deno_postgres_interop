b396b87f4473d0904a3d299aacd85bd6352ebf61
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-20 18:21:06 +0300

    fix comments

--- lib/src/partial/partial_tls_options.dart
+++ lib/src/partial/partial_tls_options.dart
@@ -1,12 +1,12 @@
-/// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions
+/// [deno-postgres@v0.17.0/TLSOptions](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions).
 class PartialTLSOptions {
-  /// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions#prop_enabled
+  /// [deno-postgres@v0.17.0/TLSOptions/enabled](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions#prop_enabled).
   final bool? isEnabled;
 
-  /// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions#prop_enfroce
+  /// [deno-postgres@v0.17.0/TLSOptions/enfroce](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions#prop_enfroce).
   final bool? isTLSEnforced;
 
-  /// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions#prop_caCertificates
+  /// [deno-postgres@v0.17.0/TLSOptions/caCertificates](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=TLSOptions#prop_caCertificates).
   final List<String>? caCertificates;
 
   /// constructor.
