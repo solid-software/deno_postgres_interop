611000cde5c34badbd23b6ac6c9701bd7d9e287d
danylo-safonov-solid <116712879+danylo-safonov-solid@users.noreply.github.com>
2023-09-20 18:13:42 +0300

    Notice (#16)
    * init
    
    * upd
    
    * init

--- lib/src/query_result.dart
+++ lib/src/query_result.dart
@@ -2,12 +2,16 @@ import 'dart:js_interop';
 import 'dart:js_util';
 
 import 'package:deno_postgres_interop/src/command_type.dart';
+import 'package:deno_postgres_interop/src/notice.dart';
 import 'package:deno_postgres_interop/src/query.dart';
 import 'package:deno_postgres_interop/src/row_description.dart';
 
 /// [deno-postgres@v0.17.0/QueryResult](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult).
 @JS()
 class QueryResult {
+  /// [deno-postgres@v0.17.0/QueryResult/warnings](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult#prop_warnings).
+  external List<Notice> get warnings;
+
   /// [deno-postgres@v0.17.0/QueryResult/constructor](https://deno.land/x/postgres@v0.17.0/query/query.ts?s=QueryResult#ctor_0).
   external Query get query;
 
