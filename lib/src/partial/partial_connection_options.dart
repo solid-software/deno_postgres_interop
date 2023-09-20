be46787155e7c39b731082b108cd631ae552f134
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-20 18:23:01 +0300

    upd

--- lib/src/partial/partial_connection_options.dart
+++ lib/src/partial/partial_connection_options.dart
@@ -1,14 +1,14 @@
-/// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions
+/// [deno-postgres@v0.17.0/ConnectionOptions](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions).
 class PartialConnectionOptions {
-  /// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions#prop_attempts
+  /// [deno-postgres@v0.17.0/ConnectionOptions](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions#prop_attempts).
   final int? attempts;
 
-  /// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions#prop_interval
+  /// [deno-postgres@v0.17.0/ConnectionOptions](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions#prop_interval).
   ///
   /// Either this or [interval] is null.
   final int Function(int previousInterval)? nextInterval;
 
-  /// https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions#prop_interval
+  /// [deno-postgres@v0.17.0/ConnectionOptions](https://deno.land/x/postgres@v0.17.0/connection/connection_params.ts?s=ConnectionOptions#prop_interval).
   ///
   /// Either this or [nextInterval] is null.
   final int? interval;
@@ -18,5 +18,5 @@ class PartialConnectionOptions {
     required this.attempts,
     required this.nextInterval,
     required this.interval,
-  });
+  }) : assert(interval == null || nextInterval == null);
 }
