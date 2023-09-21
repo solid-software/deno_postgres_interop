c87a381ab7d61724aa5d234eeb39acbf96fa3dfb
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-09-21 22:50:07 +0300

    fix

--- lib/src/errors/transaction_error.dart
+++ lib/src/errors/transaction_error.dart
@@ -6,6 +6,7 @@ import 'package:deno_postgres_interop/src/errors/postgres_error.dart';
 @JS()
 class TransactionError {
   /// [js/Error/cause](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/cause).
+  /// [deno-postgres@v0.17.0/TransactionError/constructor](https://deno.land/x/postgres@v0.17.0/client/error.ts?s=TransactionError#ctor_0).
   external PostgresError get cause;
 
   /// [js/Error/message](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/message).
