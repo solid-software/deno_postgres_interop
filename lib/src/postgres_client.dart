0ad9f1fe63d06babb246d89594aaaea9a2617b42
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-11-15 18:03:29 +0200

    revert

--- lib/src/postgres_client.dart
+++ lib/src/postgres_client.dart
@@ -4,16 +4,16 @@ import 'dart:js_util';
 import 'package:deno_postgres_interop/src/client_options.dart';
 import 'package:deno_postgres_interop/src/query_client.dart';
 
-/// [deno-postgres@v0.17.0/Client](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client).
+/// [deno-postgres@v​0.17.0/Client](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client).
 @JS('Client')
 class PostgresClient extends QueryClient {
-  /// [deno-postgres@v0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
+  /// [deno-postgres@v​0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
   external factory PostgresClient(String dbUrl);
 
-  /// [deno-postgres@v0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
+  /// [deno-postgres@v​0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
   factory PostgresClient.config(ClientOptions config) =>
       callConstructor('Client', [config]);
 
-  /// [deno-postgres@v0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
+  /// [deno-postgres@v​0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
   factory PostgresClient.empty() => callConstructor('Client', null);
 }
