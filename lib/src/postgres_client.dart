126b281bb6893f82bcb6b9efbf42137735ff88ce
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2023-11-15 17:59:03 +0200

    rename

--- lib/src/postgres_client.dart
+++ lib/src/postgres_client.dart
@@ -5,15 +5,15 @@ import 'package:deno_postgres_interop/src/client_options.dart';
 import 'package:deno_postgres_interop/src/query_client.dart';
 
 /// [deno-postgres@v0.17.0/Client](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client).
-@JS()
-class Client extends QueryClient {
+@JS('Client')
+class PostgresClient extends QueryClient {
   /// [deno-postgres@v0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
-  external factory Client(String dbUrl);
+  external factory PostgresClient(String dbUrl);
 
   /// [deno-postgres@v0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
-  factory Client.config(ClientOptions config) =>
+  factory PostgresClient.config(ClientOptions config) =>
       callConstructor('Client', [config]);
 
   /// [deno-postgres@v0.17.0/Client/constructor](https://deno.land/x/postgres@v0.17.0/mod.ts?s=Client#ctor_0).
-  factory Client.empty() => callConstructor('Client', null);
+  factory PostgresClient.empty() => callConstructor('Client', null);
 }
